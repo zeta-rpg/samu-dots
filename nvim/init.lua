ca9257d0f6e6cc0c03a0219b29167e61b00d971e
Samuel Campos <zckfr000@gmail.com>
2025-10-13 05:01:28 -0300

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
--- Configure Node.js before loading plugins
-require("config.nodejs").setup({ silent = true })
-
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-vim.opt.timeoutlen = 1000
-vim.opt.ttimeoutlen = 0
